dc50c77881d19bca7d68f1f9ad305b187f30b585
kunalbohra <kunal.bohraji.gmail.com>
2019-02-18 14:04:24 -0800

    Adding tic-tac-toe util functions

--- tic-tac-toe/utils.js
+++ tic-tac-toe/utils.js
@@ -1,12 +1,26 @@
-const empty = compose(
-  replicate(size),
-  replicate(size)
-);
-const flat = arr => arr.flat();
+const { replicate, all } = require('../common/utils/utils');
+
+const empty = size => replicate(size)(replicate(size, 'B'));
 const full = grid => all(v => v !== 'B')(grid.flat());
 
+//turn :: Grid -> Player
+const turn = grid => {
+  const flattendGrid = grid.flat();
+
+  let os = flattendGrid.filter(v => v === 'O');
+  let xs = flattendGrid.filter(v => v === 'X');
+
+  return os <= xs ? 'O' : 'X';
+};
+
+// diag :: Grid -> [Player]
+//const diag = grid =>
+// wins :: Player -> Grid -> Bool
+/* const wins = (player, grid) => {
+
+} */
 module.exports = {
   empty,
-  flat,
-  full
+  full,
+  turn
 };
---
+++ tic-tac-toe/utils.test.js
@@ -0,0 +1,24 @@
+const { empty, full, turn } = require('./utils');
+
+describe('utils test', () => {
+  xtest('an empty grid is produced by empty', () => {
+    expect(empty(3)).toEqual([
+      ['B', 'B', 'B'],
+      ['B', 'B', 'B'],
+      ['B', 'B', 'B']
+    ]);
+  });
+
+  test('full works as expected', () => {
+    expect(full([['B', 'B', 'B'], ['B', 'B', 'X'], ['X', 'O', 'B']])).toBe(
+      false
+    );
+    expect(full([['X', 'X', 'O'], ['X', 'O', 'X'], ['X', 'O', 'X']])).toBe(
+      true
+    );
+  });
+
+  test('turn works as expected', () => {
+    expect(turn([['X', 'X', 'O'], ['X', 'O', 'X'], ['X', 'O', 'X']])).toBe('O');
+  });
+});
